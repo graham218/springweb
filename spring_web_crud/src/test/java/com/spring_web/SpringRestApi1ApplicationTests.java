23e7f70a4385f80b3d39731990956ab7aceecafa
BILL GRAHAM <grahambill011@gmail.com>
2021-09-30 16:48:27 +0300

    create spring web app

---
+++ spring_web_crud/src/test/java/com/spring_web/SpringRestApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
